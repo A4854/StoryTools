2c519e0950e5eb59ec5f5e36a90461a6297c2b3e
qi.li <qi.li@vaneplus.com>
2022-01-19 17:51:09 +0800

    init repo

---
+++ StoryTools/StoryToolsFunctions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StoryTools
+{
+    public partial class StoryToolsFunctions
+    {
+        private Application excelApp = null;
+        private void StoryToolsFunctions_Load(object sender, RibbonUIEventArgs e)
+        {
+            excelApp = Globals.ThisAddIn.Application;
+        }
+
+        private void ButtonGo_Click(object sender, RibbonControlEventArgs e)
+        {
+            Range selction = excelApp.ActiveCell;
+            selction.Value = "Go";
+        }
+    }
+}
